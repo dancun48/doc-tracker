3df1edec35b384bb2755d4115b0cfdd08102b59d
dancun48 <111135378+dancun48@users.noreply.github.com>
2025-10-15 17:03:36 +0300

    Updated backend API URL for deployment

--- admin-panel/src/context/AdminContext.jsx
+++ admin-panel/src/context/AdminContext.jsx
@@ -12,7 +12,7 @@ const AdminContextProvider = (props) => {
   const [appointments, setAppointments] = useState([]);
   const [dashData, setDashData] = useState(false);
 
-  const backendUrl = import.meta.env.VITE_BACKEND_URL || import.meta.env.VITE_API_URL;
+  const backendUrl = import.meta.env.VITE_BACKEND_URL;
 
   
 
